df87b8b343123dfc510960c3076506f33bbdcf0d
Yago Machado <contato.yagomachado@outlook.com>
2024-11-12 17:32:55 -0300

    feat consumo de API

--- OBC-ReactNative/navigation/src/screens/usingApiScreen.jsx
+++ OBC-ReactNative/navigation/src/screens/usingApiScreen.jsx
@@ -1,18 +1,34 @@
 import axios from "axios";
+import theme from "../theme";
+import { useEffect, useState} from "react";
+import { Image, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Container from "../components/Container";
 import StyledButton from "../components/StyledButton";
 import StyledTitle from "../components/StyledTitle";
+import StyledText from "../components/StyledText";
 
 export default function UsingApiScreen() {
+    const [user, setUser] = useState({})
     const navigation = useNavigation()
     const navigateBack = () => {
         navigation.goBack()
     }
 
+    const fetchGithubUser = async () => {
+        const res = await axios.get("https://api.github.com/users/developeryago")
+        setUser(res.data)
+    }
+    useEffect(() => {
+        fetchGithubUser()
+    }, [])
+
     return (
         <Container>
             <StyledTitle>Consumindo API</StyledTitle>
+            <Text>{user.name}</Text>
+            <Text>{user.bio}</Text>
+            <Image source={{uri: user.avatar_url}}/>
             <StyledButton onPress={navigateBack}>Voltar</StyledButton>
         </Container>
     )
